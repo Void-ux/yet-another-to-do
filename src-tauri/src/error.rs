511b6d7ae2a1706f70616fb482b857aaa86b76d9
parzivale <40600864+parzivale@users.noreply.github.com>
2022-11-27 17:42:45 +0800

    added preliminary support for surreal db, plus some helper methods

---
+++ src-tauri/src/error.rs
@@ -0,0 +1,11 @@
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Fail to create. Cause: {0}")]
+    StoreFailToCreate(String),
+
+    #[error(transparent)]
+    Surreal(#[from] surrealdb::Error),
+
+    #[error(transparent)]
+    IO(#[from] std::io::Error),
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,30 @@
-#![cfg_attr(
+use std::{env, sync::Arc};
+
+mod database;
+mod error;
+mod prelude;
+use database::Connection;
+use prelude::*;
+
+#[cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
+    let db = Connection::new().await.unwrap();
+    let db = Arc::new(db);
+
     tauri::Builder::default()
+        .manage(db)
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+    Ok(())
 }
