8caa78d885bea287187c23836277c2b76cb7598f
Void-ux <daniel.grant2017@gmail.com>
2022-12-02 15:14:59 +0800

    refactor: rename generated function names & html/css

--- src/app.rs
+++ src/app.rs
@@ -14,19 +14,19 @@ extern "C" {
 }
 
 #[derive(Serialize, Deserialize)]
-struct GreetArgs<'a> {
+struct NewTaskArgs<'a> {
     name: &'a str,
 }
 
 #[function_component(App)]
 pub fn app() -> Html {
-    let greet_input_ref = use_ref(|| NodeRef::default());
+    let task_content = use_ref(|| NodeRef::default());
 
     let name = use_state(|| String::new());
 
-    let greet_msg = use_state(|| String::new());
+    let task_msg = use_state(|| String::new());
     {
-        let greet_msg = greet_msg.clone();
+        let task_msg = task_msg.clone();
         let name = name.clone();
         let name2 = name.clone();
         use_effect_with_deps(
@@ -38,12 +38,12 @@ pub fn app() -> Html {
 
                     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
                     let new_msg = invoke(
-                        "greet",
-                        to_value(&GreetArgs { name: &*name }).unwrap(),
+                        "create_task",
+                        to_value(&NewTaskArgs { name: &*name }).unwrap(),
                     )
                     .await;
                     log(&new_msg.as_string().unwrap());
-                    greet_msg.set(new_msg.as_string().unwrap());
+                    task_msg.set(new_msg.as_string().unwrap());
                 });
 
                 || {}
@@ -52,42 +52,23 @@ pub fn app() -> Html {
         );
     }
 
-    let greet = {
+    let create_task = {
         let name = name.clone();
-        let greet_input_ref = greet_input_ref.clone();
+        let task_content = task_content.clone();
         Callback::from(move |_| {
-            name.set(greet_input_ref.cast::<web_sys::HtmlInputElement>().unwrap().value());
+            name.set(task_content.cast::<web_sys::HtmlInputElement>().unwrap().value());
         })
     };
 
     html! {
         <main class="container">
-            <div class="row">
-                <a href="https://tauri.app" target="_blank">
-                    <img src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
-                </a>
-                <a href="https://yew.rs" target="_blank">
-                    <img src="public/yew.png" class="logo yew" alt="Yew logo"/>
-                </a>
-            </div>
-
-            <p>{"Click on the Tauri and Yew logos to learn more."}</p>
-
-            <p>
-                {"Recommended IDE setup: "}
-                <a href="https://code.visualstudio.com/" target="_blank">{"VS Code"}</a>
-                {" + "}
-                <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank">{"Tauri"}</a>
-                {" + "}
-                <a href="https://github.com/rust-lang/rust-analyzer" target="_blank">{"rust-analyzer"}</a>
-            </p>
 
             <div class="row">
-                <input id="greet-input" ref={&*greet_input_ref} placeholder="Enter a name..." />
-                <button type="button" onclick={greet}>{"Greet"}</button>
+                <input id="task-input" ref={&*task_content} placeholder="Enter a new task..." />
+                <button type="button" onclick={create_task}>{"Create Task"}</button>
             </div>
 
-            <p><b>{ &*greet_msg }</b></p>
+            <p><b>{ &*task_msg }</b></p>
         </main>
     }
 }
